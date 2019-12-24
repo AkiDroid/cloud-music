51314378b70cb37cbf44b54d61c00011a460a7ad
Aki <akidroid@163.com>
2019-12-24 11:00:42 +0800

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'space-before-function-paren': ['error', {
+      'asyncArrow': 'never',
+      'named': 'never',
+      'anonymous': 'always'
+    }],
+  }
+}
